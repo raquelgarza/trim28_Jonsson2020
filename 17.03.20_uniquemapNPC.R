773a9fc95945278fdfaa434237fb06c79c18137c
Raquel <ra7555ga@STUDMACBOOKPRO.local>
2020-06-27 19:43:58 +0200

    Individual elements mean plots for emx animals and npc experiment

--- 17.03.20_uniquemapNPC.R
+++ 17.03.20_uniquemapNPC.R
@@ -26,9 +26,6 @@ getAverage <- function(dds) {
   return(list(Mean=baseMeanPerLvl,SD=baseSDPerLvl))
   
 }
-more_10 <- function(row){
-  return(length(which(row > 10)))
-}
 meanPlot_cus <- function(exp,test,c1 = "condition 1",c2 = "condition 2",p=.05,l=0,id=F, ttl="", 
                          repel=TRUE, col1="firebrick3", col2="steelblue4", col3="black", highlights=NA){
   sign <- getSignName(x = test,p = p,l = l)
@@ -93,6 +90,7 @@ transcript_gene <- fread('/Volumes/Seagate Backup /annotation/mouse/gencode/genc
 colnames(transcript_gene) <- c('transcript_id', 'gene_id', 'gene_name', 'gene_type')
 
 # NPC ----
+# Read unique mapping quantification of TEs (repeatmasker)
 npc_TE <- fread('/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/2_readcount/TE_count_matrix_2.csv', data.table = F)
 npc_TE <- npc_TE[,colnames(npc_TE)[c(1, (which(sapply(strsplit(colnames(npc_TE)[7:ncol(npc_TE)], '/'), `[[`, 3) == 'invitro_crispr')+6))]]
 
@@ -101,29 +99,29 @@ colnames(npc_TE)[1] <- 'TE_id'
 rownames(npc_TE) <- npc_TE$TE_id
 npc_TE <- npc_TE[,-1]
 
+# Create the metadata
 npc_coldata <- data.frame(samples=colnames(npc_TE), condition=unlist(lapply(strsplit(colnames(npc_TE), '_'), `[[`, 1)))
 rownames(npc_coldata) <- npc_coldata$samples
 npc_coldata$samples <- as.character(npc_coldata$samples)
 
-# npc_TE <- merge(npc_TE, TE_classification[,c(1,2)], by.x='row.names', by.y='TE_id')
-# npc_TE <- npc_TE[,-1]
-# npc_TE <- aggregate(npc_TE[,-ncol(npc_TE)], by=list(npc_TE$TE_subfamily), FUN=sum)
-# rownames(npc_TE) <- npc_TE$Group.1
-# npc_TE <- npc_TE[,-1]
-# npc_TE <- npc_TE[which(rowSums(npc_TE) > 0),]
+# Differential TE expression analysis testing for differences between conditions (Trim28 KO vs control)
 npc_TE_dds <- DESeqDataSetFromMatrix(npc_TE[,rownames(npc_coldata)], npc_coldata, design = ~ condition)
 npc_TE_dds <- DESeq(npc_TE_dds)
 npc_TE_res <- results(npc_TE_dds)
+# Calculate average expression on normalized reads on TEs
 npc_TE_exp <- getAverage(npc_TE_dds)
-
 npc_TE_vst <- varianceStabilizingTransformation(npc_TE_dds)
+
+# Plot PCA based on TE expression
 npc_TE_pca <- plotPCA(npc_TE_vst) + theme_classic() + ggtitle("PCA - In vitro CRISPR TE expression")
 ggsave(npc_TE_pca, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/TE_pca.svg", width=20, height=20, units="cm", dpi=96)
 
+# Mean plot of individual elements
 p_TE_meanplot_npc <- meanPlot_cus(npc_TE_exp$Mean, test=npc_TE_res, l=0.5, p=0.05, c1='ko', c2='ctrl',ttl='', repel = F) + labs(title="", subtitle="")
 ggsave(p_TE_meanplot_npc, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/TE_meanplot_0.5.svg", width=20, height=20, units="cm", dpi=320)
 ggsave(p_TE_meanplot_npc, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/TE_meanplot_0.5_npc.png", dpi=320)
 
+# Read gene expression based on unique mapping 
 npc_gene <- fread('/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/2_readcount/gene_count_matrix_2.csv', data.table = F)
 npc_gene <- npc_gene[,colnames(npc_gene)[c(1, (which(sapply(strsplit(colnames(npc_gene)[7:ncol(npc_gene)], '/'), `[[`, 3) == 'invitro_crispr')+6))]]
 
@@ -132,24 +130,24 @@ colnames(npc_gene)[1] <- 'gene_id'
 rownames(npc_gene) <- npc_gene$gene_id
 npc_gene <- npc_gene[,-1]
 
-
+# Differential gene expression analysis testing for differences between conditions (Trim28 KO vs control)
 npc_gene_dds <- DESeqDataSetFromMatrix(npc_gene[,rownames(npc_coldata)], npc_coldata, design = ~ condition)
 npc_gene_dds <- DESeq(npc_gene_dds)
 npc_gene_res <- results(npc_gene_dds)
 npc_gene_exp <- getAverage(npc_gene_dds)
+
+# Plot PCA based on protein coding genes
 npc_gene_dds_protein <- npc_gene_dds[rownames(npc_gene_dds) %in% unique(subset(transcript_gene, transcript_gene$gene_type == 'protein_coding')$gene_id),]
-
 npc_gene_vst_protein <- varianceStabilizingTransformation(npc_gene_dds_protein)
-
 npc_gene_pca_protein <- plotPCA(npc_gene_vst_protein) + theme_classic() + ggtitle("PCA - In vitro CRISPR gene expression")
 ggsave(npc_gene_pca_protein, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/gene_pca_protein.svg", width=20, height=20, units="cm", dpi=96)
 
+# Mean plot of gene expression
 p_gene_meanplot_npc <- meanPlot_cus(npc_gene_exp$Mean, test=npc_gene_res, p=0.05, c1='ko', c2='ctrl',ttl='', repel = F, l=0.5)  + labs(title="", subtitle="")
 ggsave(p_gene_meanplot_npc, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/gene_meanplot_0.5.svg", width=20, height=20, units="cm", dpi=96)
 
-
-
-# emx ----
+# Emx ----
+# Read unique mapping quantification of TEs (repeatmasker)
 emx_TE <- fread('/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/2_readcount/TE_count_matrix_2.csv', data.table = F)
 emx_TE <- emx_TE[,colnames(emx_TE)[c(1, (which(sapply(strsplit(colnames(emx_TE)[7:ncol(emx_TE)], '/'), `[[`, 3) == 'invivo_bd')+6))]]
 
@@ -158,6 +156,7 @@ colnames(emx_TE)[1] <- 'TE_id'
 rownames(emx_TE) <- emx_TE$TE_id
 emx_TE <- emx_TE[,-1]
 
+# Create metadata
 emx_coldata <- data.frame(samples=colnames(emx_TE), condition=unlist(lapply(strsplit(colnames(emx_TE), '_'), `[[`, 1)))
 rownames(emx_coldata) <- emx_coldata$samples
 emx_coldata$samples <- as.character(emx_coldata$samples)
@@ -169,16 +168,20 @@ emx_coldata <- subset(emx_coldata, emx_coldata$condition != 'str')
 # rownames(emx_TE) <- emx_TE$Group.1
 # emx_TE <- emx_TE[,-1]
 
+# Differential TE expression analysis testing for differences between conditions (Trim28 KO vs control)
 emx_TE_dds <- DESeqDataSetFromMatrix(emx_TE[,rownames(emx_coldata)], emx_coldata, design = ~ condition)
 emx_TE_dds <- DESeq(emx_TE_dds)
 emx_TE_res <- results(emx_TE_dds)
 emx_TE_exp <- getAverage(emx_TE_dds)
-
 emx_TE_vst <- varianceStabilizingTransformation(emx_TE_dds)
+
+# Plot PCA based on TE expression
 emx_TE_pca <- plotPCA(emx_TE_vst) + theme_classic() + ggtitle("PCA - In vitro CRISPR TE expression")
 ggsave(emx_TE_pca, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invitro_crispr/plots/TE_pca.svg", width=20, height=20, units="cm", dpi=96)
 
+# Mean plot of individual elements 
 p_TE_meanplot_emx <- meanPlot_cus(emx_TE_exp$Mean, test=emx_TE_res, l=0.5, p=0.05, c1='ko', c2='ctrl',ttl='', repel = F) + labs(title="", subtitle="")
+
 ggsave(p_TE_meanplot_emx, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invivo_bd/plots/TE_meanplot_0.5.svg", width=20, height=20, units="cm", dpi=320)
 ggsave(p_TE_meanplot_emx, file="/Volumes/Seagate Backup /trim28/09.10.19/1_uniqmapping/invivo_bd/plots/TE_meanplot_0.5_emx.png", dpi=320)
 
